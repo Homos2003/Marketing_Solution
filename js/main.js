fda1bb13afbf6aae463877c62222e8841aa852ae
Homos2003 <homoshegazy@gmail.com>
2023-12-31 19:40:38 +0200

    add javaScript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let head = document.getElementsByClassName("head");
+
+for (let i = 0; i < head.length; i++) {
+  head[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
